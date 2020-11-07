20a1fcac6330744634b42081197ce2e5d850cd6e
Marco Ghaly <Marco.gzy@gmail.com>
2020-11-07 22:36:16 +0200

    Create new instances on navigation

---
+++ Demo/UI/Custom Views/NavigationLazyView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationLazyView.swift
+//  Demo
+//
+//  Created by Marco Ghaly on 11/7/20.
+//  Copyright © 2020 Marco Ghaly. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct NavigationLazyView<Content: View>: View {
+    let build: () -> Content
+    init(_ build: @autoclosure @escaping () -> Content) {
+        self.build = build
+    }
+    var body: Content {
+        build()
+    }
+}
--- Demo/UI/Home/HomeView.swift
+++ Demo/UI/Home/HomeView.swift
@@ -10,13 +10,13 @@ import SwiftUI
 
 struct HomeView: View {
     
-    let buttons =
-        [[("Users", Color.red, AnyView(UsersListView().environmentObject(UsersStore()))),
-          ("Posts", Color.green, AnyView(PostsListView().environmentObject(PostsStore())))],
-         [("Comments", Color.blue, AnyView(CommentsListView().environmentObject(CommentsStore()))),
-          ("ToDos", Color.yellow, AnyView(ToDosListView().environmentObject(ToDosStore())))],
-         [("Albums", Color.gray, AnyView(UsersListView().environmentObject(UsersStore()))),
-          ("Photos", Color.black, AnyView(UsersListView().environmentObject(UsersStore())))]]
+    private let buttons =
+        [[("Users", Color.red, {AnyView(UsersListView().environmentObject(UsersStore()))}),
+          ("Posts", Color.green, {AnyView(PostsListView().environmentObject(PostsStore()))})],
+         [("Comments", Color.blue, {AnyView(CommentsListView().environmentObject(CommentsStore()))}),
+          ("ToDos", Color.yellow, {AnyView(ToDosListView().environmentObject(ToDosStore()))})],
+         [("Albums", Color.gray, {AnyView(UsersListView().environmentObject(UsersStore()))}),
+          ("Photos", Color.black, {AnyView(UsersListView().environmentObject(UsersStore()))})]]
     
     var body: some View {
         NavigationView {
@@ -24,7 +24,7 @@ struct HomeView: View {
                 ForEach(buttons, id: \.self[0].0) { array in
                     HStack(spacing: 0) {
                         ForEach(array, id: \.self.0) { button in
-                            NavigationLink(destination: button.2) {
+                            NavigationLink(destination: NavigationLazyView(button.2())) {
                                 Text(button.0).frame(maxWidth: .infinity, maxHeight: .infinity).font(.title).background(button.1).foregroundColor(.white)
                             }
                         }
