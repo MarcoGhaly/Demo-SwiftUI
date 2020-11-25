c32363bc2ddf24be054850ce57fc4ec858ef1104
Marco Ghaly <Marco.gzy@gmail.com>
2020-11-25 22:20:58 +0200

    Code Enhancements

--- Demo/UI/LCE/LCEView.swift
+++ Demo/UI/LCE/LCEView.swift
@@ -9,7 +9,6 @@
 import SwiftUI
 
 struct LCEView<Content, ViewModel, Model>: View where Content: View, ViewModel: LCEViewModel<Model> {
-    
     @ObservedObject var viewModel: ViewModel
     var content: (Model) -> Content
     
@@ -21,29 +20,29 @@ struct LCEView<Content, ViewModel, Model>: View where Content: View, ViewModel: 
             case .error(let title, let message):
                 ErrorView(title: title, message: message)
             case .content:
-                viewModel.model.map {
-                    content($0)
+                viewModel.model.map { model in
+                    content(model)
                 }
             }
         }
     }
-    
 }
 
 struct LCEView_Previews: PreviewProvider {
     static var previews: some View {
-        let states: [LCEViewModel<Any>.State] = [.loading, .error(title: "Error Title", message: "Error Message"), .content]
+        let states: [LCEViewModel<String>.State] = [.loading, .error(title: "Error Title", message: "Error Message"), .content]
         return ForEach(states.indices) { index in
             getLCEView(state: states[index])
         }
         .previewLayout(.fixed(width: 400, height: 150))
     }
 
-    private static func getLCEView(state: LCEViewModel<Any>.State) -> LCEView<Text, LCEViewModel<Any>, Any> {
-        let viewModel = LCEViewModel<Any>(model: [])
+    private static func getLCEView(state: LCEViewModel<String>.State) -> LCEView<Text, LCEViewModel<String>, String> {
+        let viewModel = LCEViewModel<String>()
+        viewModel.model = "Content"
         viewModel.state = state
         return LCEView(viewModel: viewModel) { model in
-            Text("Content")
+            Text(model)
                 .font(.largeTitle)
         }
     }
