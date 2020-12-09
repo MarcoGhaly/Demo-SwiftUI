561714d96341a9d24b9208f71aae7f6053dfcf24
Marco Ghaly <Marco.gzy@gmail.com>
2020-12-10 00:16:46 +0200

    Create User Defaults Manager

---
+++ Demo/Model/Storage/UserDefaultsManager.swift
@@ -0,0 +1,26 @@
+//
+//  UserDefaultsManager.swift
+//  Demo
+//
+//  Created by Marco Ghaly on 09/12/2020.
+//  Copyright © 2020 Marco Ghaly. All rights reserved.
+//
+
+import Foundation
+
+struct UserDefaultsManager {
+    static func save<T>(value: T, forKey key: String) where T: Encodable {
+        if let data = try? JSONEncoder().encode(value) {
+            UserDefaults.standard.setValue(data, forKey: key)
+        }
+    }
+    
+    static func loadValue<T>(forKey key: String) -> T? where T: Decodable {
+        guard let data = UserDefaults.standard.value(forKey: key) as? Data else { return nil }
+        return try? JSONDecoder().decode(T.self, from: data)
+    }
+    
+    static func deleteValue(forKey key: String) {
+        UserDefaults.standard.removeObject(forKey: key)
+    }
+}
