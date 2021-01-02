19f5d518c6c10453a8d0d2035aadbb795e57d11d
Marco Ghaly <Marco.gzy@gmail.com>
2021-01-02 21:32:16 +0200

    Use Base LCE List View in Users & Posts List Views

--- Demo/UI/Posts/PostsListView.swift
+++ Demo/UI/Posts/PostsListView.swift
@@ -11,8 +11,6 @@ import SwiftUI
 struct PostsListView<DataSource: PostsDataSource>: View {
     @ObservedObject var viewModel: PostsViewModel<DataSource>
     
-    @State private var isEditMode = false
-    @State private var selectedIDs = Set<Post.ID>()
     @State private var presentAddPostView = false
     
     var body: some View {
@@ -21,14 +19,10 @@ struct PostsListView<DataSource: PostsDataSource>: View {
         ZStack(alignment: .bottom) {
             Color.clear
             
-            DefaultLCEListView(viewModel: viewModel, isEditMode: isEditMode, selectedIDs: $selectedIDs) { post in
-                cellView(forPost: post)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .animation(.none)
-            
-            if isEditMode {
-                editView
+            BaseLCEListView(viewModel: viewModel, showNavigationBarItems: userID != nil, presentAddView: $presentAddPostView) { post in
+                PostRowView(post: post)
+            } destination: { post in
+                PostDetailsView(viewModel: PostViewModel(post: post))
             }
             
             userID.map { userID in
@@ -41,74 +35,7 @@ struct PostsListView<DataSource: PostsDataSource>: View {
                 })
             }
         }
-        .edgesIgnoringSafeArea(.bottom)
         .navigationBarTitle(Text("Posts"))
-        .if(userID != nil) {
-            $0.navigationBarItems(trailing: navigationBarItems)
-        }
-    }
-    
-    private func cellView(forPost post: Post) -> some View {
-        NavigationLink(destination: NavigationLazyView(PostDetailsView(viewModel: PostViewModel(post: post)))) {
-            VStack {
-                HStack {
-                    PostRowView(post: post)
-                    if isEditMode {
-                        Image(systemName: selectedIDs.contains(post.id) ? "checkmark.circle.fill" : "circle")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .foregroundColor(.black)
-                            .padding()
-                    }
-                }
-                Divider()
-            }
-        }
-        .disabled(isEditMode)
-    }
-    
-    private var editView: some View {
-        HStack {
-            Button {
-                viewModel.deletePosts(wihtIDs: selectedIDs)
-                isEditMode = false
-            } label: {
-                VStack {
-                    Image(systemName: "trash")
-                    Text("Delete")
-                }
-            }
-        }
-        .disabled(selectedIDs.isEmpty)
-        .frame(maxWidth: .infinity)
-        .padding()
-        .cardify()
-        .transition(.move(edge: .bottom))
-    }
-    
-    private var navigationBarItems: some View {
-        HStack {
-            if viewModel.model?.isEmpty == false {
-                Button(action: {
-                    selectedIDs = []
-                    withAnimation {
-                        isEditMode.toggle()
-                    }
-                }, label: {
-                    Image(systemName: isEditMode ? "multiply.circle.fill" : "pencil.circle.fill")
-                })
-            }
-            
-            if !isEditMode {
-                Button(action: {
-                    withAnimation {
-                        presentAddPostView = true
-                    }
-                }, label: {
-                    Image(systemName: "note.text.badge.plus")
-                })
-            }
-        }
     }
 }
 
--- Demo/UI/Users/UsersListView.swift
+++ Demo/UI/Users/UsersListView.swift
@@ -11,95 +11,21 @@ import SwiftUI
 struct UsersListView<DataSource: UsersDataSource>: View {
     @ObservedObject var viewModel: UsersViewModel<DataSource>
     
-    @State private var isEditMode = false
-    @State private var selectedIDs = Set<User.ID>()
     @State private var presentAddUserView = false
-    @State private var newUser = User()
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            DefaultLCEListView(viewModel: viewModel, isEditMode: isEditMode, selectedIDs: $selectedIDs) { user in
-                cellView(forUser: user)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .animation(.none)
-            
-            if isEditMode {
-                editView
-            }
+        BaseLCEListView(viewModel: viewModel, presentAddView: $presentAddUserView) { user in
+            UserRowView(user: user)
+        } destination: { user in
+            UserDetailsView(user: user)
         }
-        .edgesIgnoringSafeArea(.bottom)
         .navigationBarTitle(Text("Users"))
-        .navigationBarItems(trailing: navigationBarItems)
         .sheet(isPresented: $presentAddUserView, content: {
             AddUserView(isPresented: $presentAddUserView) { user in
                 viewModel.add(user: user)
                 presentAddUserView = false
             }
         })
-    }
-    
-    private func cellView(forUser user: User) -> some View {
-        NavigationLink(destination: NavigationLazyView(UserDetailsView(user: user))) {
-            VStack {
-                HStack {
-                    UserRowView(user: user)
-                    if isEditMode {
-                        Image(systemName: selectedIDs.contains(user.id) ? "checkmark.circle.fill" : "circle")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .foregroundColor(.black)
-                            .padding()
-                    }
-                }
-                Divider()
-            }
-        }
-        .disabled(isEditMode)
-    }
-    
-    private var editView: some View {
-        HStack {
-            Button {
-                viewModel.deleteUsers(wihtIDs: selectedIDs)
-                isEditMode = false
-            } label: {
-                VStack {
-                    Image(systemName: "trash")
-                    Text("Delete")
-                }
-            }
-        }
-        .disabled(selectedIDs.isEmpty)
-        .frame(maxWidth: .infinity)
-        .padding()
-        .cardify()
-        .transition(.move(edge: .bottom))
-    }
-    
-    private var navigationBarItems: some View {
-        HStack {
-            if viewModel.model?.isEmpty == false {
-                Button(action: {
-                    selectedIDs = []
-                    withAnimation {
-                        isEditMode.toggle()
-                    }
-                }, label: {
-                    Image(systemName: isEditMode ? "multiply.circle.fill" : "pencil.circle.fill")
-                })
-            }
-            
-            if !isEditMode {
-                Button(action: {
-                    withAnimation {
-                        presentAddUserView = true
-                    }
-                }, label: {
-                    Image(systemName: "note.text.badge.plus")
-                })
-            }
-        }
     }
 }
 
