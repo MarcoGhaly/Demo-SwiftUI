c3282d0910bde4a6e048216e43bde3d62b39bd58
Marco Ghaly <Marco.gzy@gmail.com>
2021-01-11 01:51:41 +0200

    Add Grid View

---
+++ Demo/Base/UI/Custom Views/GridView.swift
@@ -0,0 +1,58 @@
+//
+//  GridView.swift
+//  Demo
+//
+//  Created by Marco Ghaly on 11/01/2021.
+//  Copyright © 2021 Marco Ghaly. All rights reserved.
+//
+
+import SwiftUI
+
+struct GridView<Element, Content>: View where Content: View {
+    private static var defaultSpacing: CGFloat { 10 }
+    
+    private let elements: [Element]
+    private let rows: Int
+    private let columns: Int
+    private let spacing: CGFloat
+    private let content: (Element) -> Content
+    
+    private init(elements: [Element], rows: Int, columns: Int, spacing: CGFloat, content: @escaping (Element) -> Content) {
+        self.elements = elements
+        self.rows = rows
+        self.columns = columns
+        self.spacing = spacing
+        self.content = content
+    }
+    
+    init(elements: [Element], rows: Int, spacing: CGFloat = defaultSpacing, content: @escaping (Element) -> Content) {
+        self.init(elements: elements, rows: rows, columns: elements.count / rows, spacing: spacing, content: content)
+    }
+    
+    init(elements: [Element], columns: Int, spacing: CGFloat = defaultSpacing, content: @escaping (Element) -> Content) {
+        self.init(elements: elements, rows: elements.count / columns, columns: columns, spacing: spacing, content: content)
+    }
+    
+    var body: some View {
+        VStack(spacing: spacing) {
+            ForEach(0..<rows) { row in
+                HStack(spacing: spacing) {
+                    ForEach(0..<columns) { column in
+                        let element = elements[columns * row + column]
+                        content(element)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView(elements: Array(repeating: "😃", count: 25), rows: 5) { emoji in
+            Text(emoji)
+                .font(.largeTitle)
+        }
+    }
+}
