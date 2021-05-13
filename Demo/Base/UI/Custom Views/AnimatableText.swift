01cefce76c9cabdebe8a996f3f3508601b09a954
Marco Ghaly <Marco.gzy@gmail.com>
2021-05-13 19:45:27 +0200

    Add Animatable Text & Progress Bar Views

---
+++ Demo/Base/UI/Custom Views/AnimatableText.swift
@@ -0,0 +1,54 @@
+//
+//  AnimatableText.swift
+//  Demo
+//
+//  Created by Marco Ghaly on 10/26/20.
+//  Copyright © 2020 Marco Ghaly. All rights reserved.
+//
+
+import SwiftUI
+
+struct AnimatableText: View {
+    var number: String
+    var animationDuration = 0.5
+    @State private var value = 0
+    
+    var body: some View {
+        var animate = false
+        if let number = Int(self.number) {
+            animate = true
+            DispatchQueue.main.async {
+                withAnimation(.linear(duration: self.animationDuration)) {
+                    self.value = number
+                }
+            }
+        } else {
+            DispatchQueue.main.async {
+                self.value = 0
+            }
+        }
+        
+        return Text(number)
+            .if(animate) { $0.modifier(TextAnimator(number: value)) }
+    }
+}
+
+private struct TextAnimator: AnimatableModifier {
+    var number = 0
+
+    var animatableData: CGFloat {
+        get { CGFloat(number) }
+        set { number = Int(newValue) }
+    }
+
+    func body(content: Content) -> some View {
+        Text("\(number)")
+    }
+}
+
+struct AnimatableText_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatableText(number: "0")
+            .previewLayout(.fixed(width: 100, height: 100))
+    }
+}
---
+++ Demo/Base/UI/Custom Views/ProgressBarView.swift
@@ -0,0 +1,78 @@
+//
+//  ProgressBarView.swift
+//  Demo
+//
+//  Created by Marco Ghaly on 10/20/20.
+//  Copyright © 2020 Marco Ghaly. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    typealias Border = (color: Color, width: CGFloat)
+    
+    private static let defaultBackgroundView = AnyView(Color.clear)
+    private static let defaultForegroundView = AnyView(gradient(withColors: [Color.green, Color.blue]))
+    private static let defaultAnimationDuration = 0.5
+    
+    private static func gradient(withColors colors: [Color]) -> LinearGradient {
+        LinearGradient(gradient: Gradient(colors: colors), startPoint: .leading, endPoint: .trailing)
+    }
+    
+    var value: Int
+    var backgroundView: AnyView
+    var foregroundView: AnyView
+    var border: Border?
+    var animationDuration: Double
+    
+    init(value: Int, backgroundColor: Color, foregroundColor: Color, border: Border? = nil, animationDuration: Double = ProgressBarView.defaultAnimationDuration) {
+        self.init(value: value, backgroundView: AnyView(backgroundColor), foregroundView: AnyView(foregroundColor), animationDuration: animationDuration)
+    }
+    
+    init(value: Int, backgroundColors: [Color], foregroundColors: [Color], border: Border? = nil, animationDuration: Double = ProgressBarView.defaultAnimationDuration) {
+        let backgroundGradient = ProgressBarView.gradient(withColors: backgroundColors)
+        let foregroundGradient = ProgressBarView.gradient(withColors: foregroundColors)
+        self.init(value: value, backgroundView: AnyView(backgroundGradient), foregroundView: AnyView(foregroundGradient), animationDuration: animationDuration)
+    }
+    
+    init(value: Int, backgroundView: AnyView = ProgressBarView.defaultBackgroundView, foregroundView: AnyView = ProgressBarView.defaultForegroundView, border: Border? = nil, animationDuration: Double = ProgressBarView.defaultAnimationDuration) {
+        self.value = value
+        self.backgroundView = backgroundView
+        self.foregroundView = foregroundView
+        self.border = border
+        self.animationDuration = animationDuration
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .background(self.backgroundView)
+                
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .background(self.foregroundView)
+                    .frame(width: geometry.size.width * CGFloat(self.value) / 100)
+                    .animation(.linear(duration: self.animationDuration))
+                
+                if self.border != nil {
+                    RoundedRectangle(cornerRadius: geometry.size.height / 2)
+                        .stroke(self.border!.color, lineWidth: self.border!.width)
+                }
+            }
+            .cornerRadius(geometry.size.height / 2)
+        }
+    }
+}
+
+struct ProgressBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        let values = [0, 25, 50, 75, 100]
+        return ForEach(values, id: \.self) { value in
+            ProgressBarView(value: value)
+                .previewLayout(.fixed(width: 400, height: 40))
+                .padding(10)
+        }
+    }
+}
