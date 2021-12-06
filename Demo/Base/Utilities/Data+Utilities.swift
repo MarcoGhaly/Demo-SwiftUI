6ce002bff131231cf35d864c9e34ec57f11f8ddd
Marco Ghaly <Marco.gzy@gmail.com>
2021-12-06 18:29:09 +0200

    Add Data Utilities

---
+++ Demo/Base/Utilities/Data+Utilities.swift
@@ -0,0 +1,21 @@
+//
+//  Data+Utilities.swift
+//  Demo
+//
+//  Created by Marco Ghaly on 06/12/2021.
+//  Copyright © 2021 Marco Ghaly. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+        return self.map { String(format: format, $0) }.joined()
+    }
+}
