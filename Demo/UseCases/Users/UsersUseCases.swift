ec87acc8e033b3d77e34ed3ffbe9c2f5100853ab
Marco Ghaly <marco.ghaly@hellofresh.com>
2024-12-24 20:50:44 +0100

    Remove static coordinates

--- Demo/UseCases/Users/UsersUseCases.swift
+++ Demo/UseCases/Users/UsersUseCases.swift
@@ -14,7 +14,6 @@ class UsersUseCases: UsersUseCasesProtocol {
     
     func getUsers(page: Int?, limit: Int?) -> AnyPublisher<[User], AppError> {
         var publisher = dataSource.getRemoteUsers(page: page, limit: limit)
-        publisher = addCoordinates(to: publisher)
         
         if page == 1 {
             let localPublisher = dataSource.getLocalUsers()
@@ -65,34 +64,4 @@ class UsersUseCases: UsersUseCasesProtocol {
             .delay(for: .seconds(1), scheduler: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
-}
-
-private extension UsersUseCases {
-    func addCoordinates(to publisher: AnyPublisher<[User], DataError>) -> AnyPublisher<[User], DataError> {
-        publisher.map { users in
-            let coordinates = Self.coordinates
-            var counter = 0
-            users.forEach { user in
-                user.address?.geo = coordinates[counter % coordinates.count]
-                counter += 1
-            }
-            return users
-        }
-        .eraseToAnyPublisher()
-    }
-}
-
-private extension UsersUseCases {
-    static let coordinates = [
-        (29.979250, 31.134222), // Great Pyramid at Giza
-        (40.689250, -74.044444), // Statue of Liberty
-        (48.858361, 2.294444), // Eiffel Tower
-        (55.752500, 37.623056), // St. Basil's Cathedral
-        (41.005389, 28.976778), // Blue Mosque
-        (27.175056, 78.042111), // Taj Mahal
-        (37.820111, -122.478278), // Golden Gate Bridge
-        (38.624639, -90.184806), // Gateway Arch
-        (52.516250, 13.377667), // Brandenburg Gate
-        (25.197167, 55.274361) // Burj Khalifa
-    ].map { Geo(lat: String($0.0), lng: String($0.1)) }
 }
